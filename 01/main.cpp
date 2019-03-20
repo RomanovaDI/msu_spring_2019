37e5ead319283780c9774ab0c8c793feaa6c3a75
Daria Romanova <romanovadi@gmail.com>
2019-03-20 20:40:26 +0300

    All comments corrected
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- 01/main.cpp
+++ 01/main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 using namespace std;
 
-int isprostoe(int n)
+#define MAX 100000
+
+int isPrimeNumber(int n)
 {
-	int d;
 	if (n < 2) return 0;
 	if (n == 2) return 1; // 2 - простое
 	if (n % 2 == 0) return 0; // четное -> не простое
-	d = 3; // начальный делитель
+	int d = 3; // начальный делитель
 	while (d * d <= n) {
 		if (n % d == 0) return 0;
 			d += 2; // следующий делитель
@@ -21,19 +22,19 @@ int main(int argc, char* argv[])
 	if ((! (argc % 2)) || (argc < 3))
 		return -1;
 	for (int i = 1; i < argc; i += 2) {
-		int s = std::atoi(argv[i]);
-		if ((s < 0) || (s > 100000))
+		int start = std::atoi(argv[i]);
+		if ((start < 0) || (start > MAX))
 			return -1;
-		int f = std::atoi(argv[i + 1]);
-		if ((f < 0) || (f > 100000))
+		int finish = std::atoi(argv[i + 1]);
+		if ((finish < 0) || (finish > MAX))
 			return -1;
 		int count = 0;
-		for (int j = 0 ; j < sizeof(Data); j++) {
-			if (Data[j] < s)
+		for (int j = 0 ; j < Size; j++) {
+			if (Data[j] < start)
 				continue;
-			if (Data[j] > f)
+			if (Data[j] > finish)
 				break;
-			if (isprostoe(Data[j]))
+			if (isPrimeNumber(Data[j]))
 				count++;
 		}
 		std::cout << count << endl;
